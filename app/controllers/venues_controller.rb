6f49d444a97d93ffa8278b0afb59c55afbbdd5db
Chris Zempel <zempel@efeqdev.com>
2014-11-16 15:18:51 -0600

    venue report cards

--- app/controllers/venues_controller.rb
+++ app/controllers/venues_controller.rb
@@ -5,6 +5,7 @@ class VenuesController < ApplicationController
   end
 
   def show
+    @venue = Venue.find(params[:id])
   end
 
   def create
---
+++ app/models/venue_report_card.rb
@@ -0,0 +1,10 @@
+class VenueReportCard
+  attr_accessor :venue
+
+  def initialize(venue)
+    @venue = venue
+  end
+
+  def wifi_rating
+  end
+end
